19cd846348a3ad34fad36b4acf9d647ff731f0cc
chargE <ahujaarush@gmail.com>
2017-12-20 11:34:42 +0530

    Added DD Library

---
+++ DifferentialDrive.cpp
@@ -0,0 +1,89 @@
+#include "Arduino.h"
+#include "DifferentialDrive.h"
+
+/*  
+*   Constructor Parameters 
+*   ( pins[rightMotorEnable, rightMotorDirF, rightMotorDirB,leftMotorEnable, leftMotorDirF, leftMotorDirB], rightDefaultSpeed, leftDefaultSpeed ) 
+*   
+*/    
+
+DifferentialDrive::DifferentialDrive() {
+  
+}
+
+DifferentialDrive::DifferentialDrive(int pins[],int rDS, int lDS) {
+  rightMotorEnable = pins[0];
+  rightMotorDirF = pins[1];
+  rightMotorDirB = pins[2];
+  
+  leftMotorEnable = pins[3];
+  leftMotorDirF = pins[4];
+  leftMotorDirB = pins[5];
+
+  rightDefaultSpeed = rDS;
+  leftDefaultSpeed = lDS;
+
+  pinMode(rightMotorEnable,OUTPUT);
+  pinMode(rightMotorDirF,OUTPUT);
+  pinMode(rightMotorDirB,OUTPUT);
+
+  pinMode(leftMotorEnable,OUTPUT);
+  pinMode(leftMotorDirF,OUTPUT);
+  pinMode(leftMotorDirB,OUTPUT);
+  
+}
+
+void DifferentialDrive::forward() {
+  //      Right Motor 
+  analogWrite(rightMotorEnable, rightDefaultSpeed);
+  digitalWrite(rightMotorDirF, HIGH);
+  digitalWrite(rightMotorDirB, LOW);
+  //      Left Motor 
+  analogWrite(leftMotorEnable, leftDefaultSpeed);
+  digitalWrite(leftMotorDirF, HIGH);
+  digitalWrite(leftMotorDirB, LOW);      
+}
+
+void DifferentialDrive::backward() {
+  //      Right Motor 
+  analogWrite(rightMotorEnable, rightDefaultSpeed);
+  digitalWrite(rightMotorDirF, LOW);
+  digitalWrite(rightMotorDirB, HIGH);
+  //      Left Motor 
+  analogWrite(leftMotorEnable, leftDefaultSpeed);
+  digitalWrite(leftMotorDirF, LOW);
+  digitalWrite(leftMotorDirB, HIGH);      
+}
+
+void DifferentialDrive::left() {
+  //      Right Motor 
+  analogWrite(rightMotorEnable, rightDefaultSpeed);
+  digitalWrite(rightMotorDirF, LOW);
+  digitalWrite(rightMotorDirB, HIGH);
+  //      Left Motor 
+  analogWrite(leftMotorEnable, leftDefaultSpeed);
+  digitalWrite(leftMotorDirF, HIGH);
+  digitalWrite(leftMotorDirB, LOW);      
+}
+
+void DifferentialDrive::right() {
+  //      Right Motor 
+  analogWrite(rightMotorEnable,rightDefaultSpeed);
+  digitalWrite(rightMotorDirF,HIGH);
+  digitalWrite(rightMotorDirB,LOW);
+  //      Left Motor 
+  analogWrite(leftMotorEnable,leftDefaultSpeed);
+  digitalWrite(leftMotorDirF,LOW);
+  digitalWrite(leftMotorDirB,HIGH);      
+}
+
+void DifferentialDrive::stop() {
+  //      Right Motor 
+  analogWrite(rightMotorEnable,0);
+  digitalWrite(rightMotorDirF,0);
+  digitalWrite(rightMotorDirB,LOW);
+  //      Left Motor 
+  analogWrite(leftMotorEnable,0);
+  digitalWrite(leftMotorDirF,LOW);
+  digitalWrite(leftMotorDirB,LOW);      
+}
---
+++ DifferentialDrive.h
@@ -0,0 +1,40 @@
+
+#ifndef DifferentialDrive_h
+#define DifferentialDrive_h
+
+
+#include "Arduino.h"
+
+enum direction {
+    FORWARD,
+    BACKWARD,
+    STOP,
+    LEFT,
+    RIGHT
+};
+
+class DifferentialDrive {
+    
+private:
+    int rightMotorEnable;
+    int rightMotorDirF;
+    int rightMotorDirB;
+    
+    int leftMotorEnable;
+    int leftMotorDirF;
+    int leftMotorDirB;
+
+    int leftDefaultSpeed;
+    int rightDefaultSpeed;
+
+public:
+    DifferentialDrive();
+    DifferentialDrive(int[], int, int);
+    void forward();
+    void backward();
+    void left();
+    void right();
+    void stop();
+};
+
+#endif
